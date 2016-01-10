94f9b2b2ffd6b390dd758ae268bcd95167420131
Michael Correale <michaelpcorreale@gmail.com>
2016-01-09 20:57:42 -0500

    Update for MAMP users

--- config/database.php
+++ config/database.php
@@ -62,6 +62,7 @@ return [
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
 			'strict'    => false,
+			'unix_socket' => '/Applications/MAMP/tmp/mysql/mysql.sock',
 		],
 
 		'pgsql' => [
